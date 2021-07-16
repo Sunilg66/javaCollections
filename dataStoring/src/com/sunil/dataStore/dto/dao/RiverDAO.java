10b5725de9fc914fec3854023dd4cb03537189c6
Sunilg66 <sunilkg251@gmail.com>
2021-07-16 22:02:23 +0530

    adding file

---
+++ dataStoring/src/com/sunil/dataStore/dto/dao/RiverDAO.java
@@ -0,0 +1,9 @@
+package com.sunil.dataStore.dto.dao;
+
+import com.sunil.dataStore.dto.RiverDTO;
+
+public interface RiverDAO {
+	
+	boolean save(RiverDTO dto);
+
+}
---
+++ dataStoring/src/com/sunil/dataStore/dto/dao/RiverDAOImpl.java
@@ -0,0 +1,13 @@
+package com.sunil.dataStore.dto.dao;
+
+import com.sunil.dataStore.dto.RiverDTO;
+
+public class RiverDAOImpl implements RiverDAO{
+	
+	@Override 
+	public boolean save(RiverDTO dto) {
+		return false;
+	}
+	
+
+}
